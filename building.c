d835e0595dbb637d4ddebf3d761237190a8086b4
Trippley <trippley@doujinscene.com>
2014-01-21 11:41:07 +0100

    building configpath

---
+++ building.c
@@ -0,0 +1,9 @@
+﻿#include "building.h"
+
+const char *get_prefix() {
+        return "./data/";
+}
+
+const char *get_config_path() {
+        return ".";
+}
---
+++ building.h
@@ -0,0 +1,7 @@
+﻿#ifndef BUILDING_H
+#define BUILDING_H
+
+const char *get_prefix(void);
+const char *get_config_path(void);
+
+#endif
